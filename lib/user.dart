245e41d2263a7aee7b54e7ec2cce91c3756e2a5c
thomas ngr <tom.ngr@zaclys.net>
2021-09-01 13:28:47 +0200

    user carousel

--- lib/user.dart
+++ lib/user.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // ignore: unused_import
-import 'drawer.dart';
+//import 'drawer.dart';
+import 'infos.dart';
 
 // Map user
 Map user = {
@@ -12,24 +13,47 @@ Map user = {
 };
 
 class PageUser extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('User'),
       ),
-      // drawer commented to let Flutter automatically add "back" arrow.
-      //drawer: CustomDrawer(),
       body: Profile(),
     );
   }
 }
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({
     Key? key,
   }) : super(key: key);
+
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+
+  // generate user carousel
+  generateUserCarousel(context, favourites) {
+    List<Widget> posterCarousel = [];
+    for (int i in favourites) {
+      posterCarousel.add(
+        Padding(
+          padding: EdgeInsets.all(15),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {selectedFilm = i;});
+              Navigator.pushNamed(context, 'detail');
+            },
+            child: Image.network(films[i]['posterLink'])
+          )
+        )
+      );
+    }
+    return posterCarousel;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +62,9 @@ class Profile extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
         child: Column(
           children: [
-            // card
+            /*
+             * CARD
+             */
             Material(
               elevation: 8,
               borderRadius: BorderRadius.circular(25),
@@ -46,12 +72,14 @@ class Profile extends StatelessWidget {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(25),
                 ),
-                height: MediaQuery.of(context).size.height * .5,
+                height: MediaQuery.of(context).size.height * .75,
                 child: Column(
                   children: [
                     Stack(
                       children: [
-                        // background image
+                         /*
+                          * BG image
+                          */
                         Container(
                           height: MediaQuery.of(context).size.height * .15,
                           decoration: BoxDecoration(
@@ -63,7 +91,9 @@ class Profile extends StatelessWidget {
                                       'https://images.omerlocdn.com/resize?url=https%3A%2F%2Fgcm-v2.omerlocdn.com%2Fproduction%2Fglobal%2Ffiles%2Fimage%2F9872a05d-e2cf-4126-849a-e084a416fc47.jpg&width=1024&type=webp'),
                                   fit: BoxFit.cover)),
                         ),
-                        // avatar
+                         /*
+                          * AVATAR
+                          */
                         Padding(
                           padding: EdgeInsets.only(top: 80, left: 20),
                           child: CircleAvatar(
@@ -76,7 +106,9 @@ class Profile extends StatelessWidget {
                             ),
                           ),
                         ),
-                        // user infos
+                        /*
+                          * USER INFO
+                          */
                         Padding(
                             padding: EdgeInsets.only(top: 130, left: 120),
                             child: Column(
@@ -97,7 +129,6 @@ class Profile extends StatelessWidget {
                     SizedBox(
                       height: 30,
                     ),
-                    // other infos
                     Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
@@ -113,9 +144,19 @@ class Profile extends StatelessWidget {
                                 fontSize: 18, fontWeight: FontWeight.bold)),
                       ],
                     ),
-                    SizedBox(
-                      height: 30,
+                    /*
+                    * CAROUSEL
+                    */
+                    SingleChildScrollView(
+                      child: SizedBox(
+                        height: 300,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: generateUserCarousel(context, user['favourite']),
+                        ),
+                      ),
                     ),
+
 
                   ],
                 ),
